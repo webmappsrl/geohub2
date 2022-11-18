2bfc13b1328a515b66995d9236a3d60d1d039b4d
Pedram Katanchi <pedramkat@gmail.com>
2022-11-18 17:14:09 +0100

    bash one click

---
+++ docker/init-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ../
+cp .env.example .env
+docker compose up -d
+
+echo "Do you want to install and activate xdebug? y/n"
+read xdebug
+
+if [[ $xdebug = y ]]
+then
+    bash docker/configs/phpfpm/init-xdebug.sh
+fi
