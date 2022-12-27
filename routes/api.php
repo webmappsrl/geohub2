051b455ad320a25bacdaeb8863a8cd5ad8aa4789
marco <m.baroncini@cyberway.cloud>
2022-12-27 11:37:42 +0100

    Init sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Public routes
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+
+
+
+// Protected routes
+Route::post('/register', [AuthController::class, 'register'])
+    ->middleware(['auth:sanctum', 'abilities:create-users']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
