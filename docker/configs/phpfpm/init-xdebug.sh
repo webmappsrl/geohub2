bf29ca11fbc19ddff3231d332fd4aeccf0bb6280
marco <m.baroncini@cyberway.cloud>
2022-12-27 11:40:44 +0100

    init develop branch

--- docker/configs/phpfpm/init-xdebug.sh
+++ docker/configs/phpfpm/init-xdebug.sh
@@ -4,4 +4,4 @@ source .env
 containerName="php81_$APP_NAME"
 docker exec -it -u 0 $containerName pecl install xdebug
 
-docker cp ./docker/configs/phpfpm/init-xdebug.sh $containerName:/usr/local/etc/php/conf.d
+docker cp ./docker/configs/phpfpm/xdebug.ini $containerName:/usr/local/etc/php/conf.d/.
