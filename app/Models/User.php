446390b2a4ecf8ca8961029ba5495c27745faff5
marco <m.baroncini@cyberway.cloud>
2022-12-29 16:23:09 +0100

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Wm\WmPackage\Model\User as ModelUser;
 
-class User extends Authenticatable
+class User extends ModelUser
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
