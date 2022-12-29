a4601ab92be881afe205ef190f932426b81f0d79
marco <m.baroncini@cyberway.cloud>
2022-12-29 11:39:10 +0100

    Test store api

---
+++ tests/Feature/StoreApiTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Tests\TestCase;
+use Illuminate\Http\Client\Request;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+use Wm\WmPackage\Facades\HoquClient;
+
+
+class StoreApiTest extends TestCase
+{
+  /**
+   * A basic test example.
+   *
+   * @return void
+   */
+  public function test_store_to_hoqu_http_call()
+  {
+    $params = [
+      'job_name' => 'AddLocationsToPoint',
+      'geometry' => json_encode(['type' => 'Point', 'coordinates' => [1, 2]])
+    ];
+
+    $url = env('HOQU_URL');
+
+    Http::fake([
+      $url . '*' => Http::sequence()->pushStatus(200)
+    ]);
+
+    HoquClient::store($params);
+
+    Http::assertSentCount(1);
+
+    #https://laravel.com/docs/9.x/http-client#recording-requests-and-responses
+    $recorded = Http::recorded(function (Request $request, Response $response) use ($url, $params) {
+      return $request->hasHeader('content', 'application/jsno') &&
+        $request->url() == $url . '/api/store'  &&
+        $request->body() == $params &&
+        $response->successful();
+    });
+  }
+}
