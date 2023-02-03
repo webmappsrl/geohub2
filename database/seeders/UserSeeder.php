3daa362afc77196478b024caa16c0e69aadcfc3c
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-02-03 16:48:27 +0100

    UserSeeder created

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Webmapp',
+            'email' => 'team@webmapp.it',
+            'password' => bcrypt('webmapp'),
+            'is_admin' => true,
+        ])->markEmailAsVerified();
+
+        User::factory()->create([
+            'name' => 'Editor',
+            'email' => 'editor@webmapp.it',
+            'password' => bcrypt('webmapp'),
+            'is_editor' => true,
+        ])->markEmailAsVerified();
+
+        User::factory()->create([
+            'name' => 'Contributor',
+            'email' => 'contributor@webmapp.it',
+            'password' => bcrypt('webmapp'),
+        ])->markEmailAsVerified();
+    }
+}
