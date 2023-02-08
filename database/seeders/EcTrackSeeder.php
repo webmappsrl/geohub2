2c6fc885b3a7537c5a50f347498cfc2a5c552230
RubensGarofalo <rubensgiuseppegarofalo@gmail.com>
2023-02-08 15:10:24 +0100

    Database seeder updated

--- database/seeders/EcTrackSeeder.php
+++ database/seeders/EcTrackSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\EcTrack;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,6 @@ class EcTrackSeeder extends Seeder
      */
     public function run()
     {
-        //
+        EcTrack::factory()->count(100)->create();
     }
 }
