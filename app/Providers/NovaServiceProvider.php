4c8894cd52498533eb7604644e0cbca7886d2b6f
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-02-12 02:17:16 +0100

    Created Track and POIs nova menus under the "Content" section

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,8 +2,15 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+
+use App\Nova\EcPoi;
+use App\Nova\EcTrack;
 use Laravel\Nova\Nova;
+use Illuminate\Http\Request;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Dashboards\Main;
+use Laravel\Nova\Menu\MenuSection;
+use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
@@ -16,6 +23,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function boot()
     {
         parent::boot();
+
+        $this->getCustomMenu();
     }
 
     /**
@@ -26,9 +35,9 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function routes()
     {
         Nova::routes()
-                ->withAuthenticationRoutes()
-                ->withPasswordResetRoutes()
-                ->register();
+            ->withAuthenticationRoutes()
+            ->withPasswordResetRoutes()
+            ->register();
     }
 
     /**
@@ -77,5 +86,19 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function register()
     {
         //
+    }
+
+
+    private function getCustomMenu()
+    {
+        Nova::mainMenu(function (Request $request) {
+            return [
+                MenuSection::dashboard(Main::class),
+                MenuSection::make('Content', [
+                    MenuItem::resource(EcTrack::class),
+                    MenuItem::resource(EcPoi::class),
+                ])->collapsable(),
+            ];
+        });
     }
 }
