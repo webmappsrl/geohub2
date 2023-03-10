0cadf2543e997f69569895cebef4d2378fc3b338
Pedram Katanchi <pedramkat@gmail.com>
2023-03-10 11:44:25 +0100

    nova package tool

--- database/migrations/2023_03_09_111301_create_taxonomy_themes_table.php
+++ database/migrations/2023_03_09_111301_create_taxonomy_themes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('taxonomy_themes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name')->nullable();
+            $table->string('name');
             $table->string('identifier')->unique();
         });
     }
