e5a12ca3f37bfbcf8c00092169dde63c663289c0
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-10 15:40:38 +0100

    Taxonomy Seeder modified to add EcTracks relation

--- database/seeders/TaxonomyThemeSeeder.php
+++ database/seeders/TaxonomyThemeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\EcTrack;
 use App\Models\TaxonomyTheme;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +14,13 @@ class TaxonomyThemeSeeder extends Seeder
      */
     public function run(): void
     {
-        TaxonomyTheme::factory()->count(100)->create();
+        if (EcTrack::count() === 0) {
+            EcTrack::factory(50)->create();
+        }
+
+        TaxonomyTheme::factory(100)->create();
+        foreach (EcTrack::all() as $track) {
+            $track->taxonomyThemes()->attach(TaxonomyTheme::inRandomOrder()->limit(rand(1, 3))->get());
+        }
     }
 }
