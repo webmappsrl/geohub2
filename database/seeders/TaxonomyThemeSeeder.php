74347ee0318555e0ea123f3bd91049d47f65715a
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-10 15:48:53 +0100

    Taxonomy Seeder modified to add EcPois relation

--- database/seeders/TaxonomyThemeSeeder.php
+++ database/seeders/TaxonomyThemeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\EcPoi;
 use App\Models\EcTrack;
 use App\Models\TaxonomyTheme;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -17,10 +18,18 @@ class TaxonomyThemeSeeder extends Seeder
         if (EcTrack::count() === 0) {
             EcTrack::factory(50)->create();
         }
+        if (EcPoi::count() === 0) {
+            EcPoi::factory(50)->create();
+        }
 
         TaxonomyTheme::factory(100)->create();
+
         foreach (EcTrack::all() as $track) {
             $track->taxonomyThemes()->attach(TaxonomyTheme::inRandomOrder()->limit(rand(1, 3))->get());
+        }
+
+        foreach (EcPoi::all() as $poi) {
+            $poi->taxonomyThemes()->attach(TaxonomyTheme::inRandomOrder()->limit(rand(1, 3))->get());
         }
     }
 }
