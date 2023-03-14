ff9182e3278e54fe3404b86f0ad1da8f3cde3af5
Pedram Katanchi <pedramkat@gmail.com>
2023-03-14 12:56:03 +0100

    update deploy dev

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -7,16 +7,15 @@ echo "Deployment started ..."
 # if already is in maintenance mode
 (php artisan down) || true
 
-# Pull the latest version of the app
-# git pull origin develop
 
 # Install composer dependencies
 composer install  --no-interaction --prefer-dist --optimize-autoloader
 # php artisan nova:install
 
-# Run database migrations
-php artisan migrate:fresh --seed
-
+# TODO
+php artisan db:wipe
+php artisan db:restore
+php artisan migrate
 
 # Clear the old cache
 php artisan clear-compiled
