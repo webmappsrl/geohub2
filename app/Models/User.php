c5f3b985d0abef751b428fbe034af3c235176d5a
marco <m.baroncini@cyberway.cloud>
2025-04-23 14:23:47 +0200

    fix: user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Wm\WmPackage\Models\User as WmUser;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends WmUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
