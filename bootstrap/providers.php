b00167c0ae7bc246ce0c626f577e9b9fdf439d33
marco <m.baroncini@cyberway.cloud>
2025-04-23 16:22:42 +0200

    fix: update providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\NovaServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
